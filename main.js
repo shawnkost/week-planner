b37d674bcd7c373d90a79938816f6275584c2f0b
michaelvalenzuela <20mvalen@gmail.com>
2020-12-07 11:40:07 -0800

    Michael did stuff

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 var $modal = document.querySelector('.modal-container');
 
-var $button1 = document.querySelector('#button1');
+var $addEntryBtn = document.querySelector('#add-entry-btn');
 
-$button1.addEventListener('click', openModal);
+$addEntryBtn.addEventListener('click', openModal);
 
 function openModal(event) {
 
